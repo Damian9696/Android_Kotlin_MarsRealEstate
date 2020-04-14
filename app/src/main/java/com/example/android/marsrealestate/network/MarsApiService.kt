323d9cce4508da965fd20db4a49718b764041dd6
Damian <damian.ogorek.1996@gmail.com>
2020-04-14 11:07:32 +0200

    Added Coroutines and Deferred.

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,9 +17,10 @@
 
 package com.example.android.marsrealestate.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -36,6 +37,8 @@ private val moshi = Moshi.Builder()
 //Retrofit object
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
+        //replacing the callback and interfaces with coroutines
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .baseUrl(BASE_URL)
         .build()
 
@@ -43,7 +46,8 @@ private val retrofit = Retrofit.Builder()
 // properties
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    //Deferred -> It's a kind of coroutines job that can directly return a result
+    fun getPropertiesAsync(): Deferred<List<MarsProperty>>
 }
 
 //MarsApi to expose the Retrofit service to the rest of the app
